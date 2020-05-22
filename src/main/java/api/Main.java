479af849fccd35885e75c6e7053168d013ac21e1
sweetlover47 <pandaforever47@gmail.com>
2020-05-22 14:13:41 +0700

    изменила конфиг подключения

--- src/main/java/api/Main.java
+++ src/main/java/api/Main.java
@@ -9,14 +9,14 @@ import java.awt.*;
 import java.io.IOException;
 
 public class Main {
-    private static final String url = "jdbc:postgresql://localhost:5432/travelagency";//"jdbc:postgresql://localhost:54325/postgres";
-    private static final String user = "nastya"; //
-    private static final String password = "123456"; //"mysecretpassword";
+    private static final String url = /*"jdbc:postgresql://localhost:5432/travelagency";/*/"jdbc:postgresql://localhost:54325/postgres";
+    private static final String user = "postgres";
+    private static final String password = "mysecretpassword";
 
     public static final int SCREEN_WIDTH = Toolkit.getDefaultToolkit().getScreenSize().width;
     public static final int SCREEN_HEIGHT = Toolkit.getDefaultToolkit().getScreenSize().height;
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         Flyway flyway = Flyway.configure().dataSource(url, user, password).load();
         flyway.migrate();
         new ChooseRole(new Controller(new Service(new RepositoryPostgres())));
